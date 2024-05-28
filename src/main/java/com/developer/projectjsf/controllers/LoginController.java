30596cb476d7e5ec0e17e3ff984460acea4f6233
Cesar Aramayo <cesar_aramayo@hotmail.es>
2024-05-28 08:35:38 -0300

    Nombre del proyecto: Project-JSF Version 1.1 Descripción : Se crea Login del aplicativo y se redirecciona a la pantalla principal.

---
+++ src/main/java/com/developer/projectjsf/controllers/LoginController.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.developer.projectjsf.controllers;
+
+import java.io.IOException;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+/**
+ * @author Cesar Clase que permite controlar el funcionamiento con la pantalla
+ *         de login.xhtml
+ */
+@ManagedBean
+public class LoginController {
+
+	private String usuario;
+
+	private String password;
+
+	public void ingresar() {
+		System.out.println("Usuario: " + usuario);
+
+		if (usuario.equals("cesar") && password.equals("12345")) {
+             try {
+				this.redireccionar("principal.xhtml");
+			} catch (IOException e) {
+				FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario",
+						new FacesMessage(FacesMessage.SEVERITY_ERROR, "La pagina no existe!", ""));
+				e.printStackTrace();
+			}
+		} else {
+			FacesContext.getCurrentInstance().addMessage("formLogin:txtUsuario",
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Usuario y/o contraseña incorrectos", ""));
+		}
+	}
+
+	private void redireccionar(String pagina) throws IOException {
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+		ec.redirect(pagina);
+	}
+
+	/**
+	 * @return the usuario
+	 */
+	public String getUsuario() {
+		return usuario;
+	}
+
+	/**
+	 * @param usuario the usuario to set
+	 */
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
